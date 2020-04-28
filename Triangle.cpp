310c754dc18d3844febc66c819090a658d4c242a
Nick <legokol3@gmail.com>
2020-04-29 00:31:27 +0300

    Functional triangle added

---
+++ Triangle.cpp
@@ -0,0 +1,32 @@
+#include "Triangle.h"
+
+Triangle::Triangle(float x1, float y1, float x2, float y2, float x3, float y3) : Object(x1, y1), a(x2 - x1, y2 - y1),
+                                                                                 b(x3 - x1, y3 - y1) {
+    shape[0].position = sf::Vector2f(loc.getX(), loc.getY());
+    shape[1].position = sf::Vector2f(loc.getX() + a.getX(), loc.getY() + a.getY());
+    shape[2].position = sf::Vector2f(loc.getX() + b.getX(), loc.getY() + b.getY());
+}
+
+void Triangle::display(sf::RenderWindow &w) const {
+    w.draw(shape);
+}
+
+void Triangle::fill(int r, int g, int b) {
+    shape[0].color = sf::Color(r, g, b);
+    shape[1].color = sf::Color(r, g, b);
+    shape[2].color = sf::Color(r, g, b);
+}
+
+void Triangle::setLoc(float x, float y) {
+    loc.setX(x);
+    loc.setY(y);
+    shape[0].position = sf::Vector2f(loc.getX(), loc.getY());
+    shape[1].position = sf::Vector2f(loc.getX() + a.getX(), loc.getY() + a.getY());
+    shape[2].position = sf::Vector2f(loc.getX() + b.getX(), loc.getY() + b.getY());
+}
+
+void Triangle::rotate(float angle) {
+    a.rotate_R(angle);
+    b.rotate_R(angle);
+    setLoc(loc.getX(), loc.getY());
+}
---
+++ Triangle.h
@@ -0,0 +1,23 @@
+#ifndef PRIMITIVES_TRIANGLE_H
+#define PRIMITIVES_TRIANGLE_H
+
+#include "Object.h"
+
+class Triangle : public Object {
+protected:
+    sf::VertexArray shape = sf::VertexArray(sf::Triangles, 3);
+    Vector2D a, b;
+public:
+    Triangle(float x1, float y1, float x2, float y2, float x3, float y3);
+
+    void display(sf::RenderWindow &w) const override;
+
+    void fill(int r, int g, int b) override;
+
+    void setLoc(float x, float y) override;
+
+    void rotate(float angle) override;
+};
+
+
+#endif //PRIMITIVES_TRIANGLE_H
